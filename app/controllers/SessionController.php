48a72724780f4f56ffdd263e4ea871d9b65ebaf9
devfake <devfakeplus@googlemail.com>
2014-12-15 20:02:39 +0100

    small changes

--- app/controllers/SessionController.php
+++ app/controllers/SessionController.php
@@ -1,11 +1,14 @@
 <?php
 
-    class SessionController extends vume\Controller {
-        public function getConfig() {
-            $res       = ["pusher_key" => pusher_key,
-                          "pusher_id" => pusher_id,
-                          "pusher_secret" => pusher_secret,
-                          "username" => session("username")->get()];
-            echo json_encode($res);
-        }
+  class SessionController extends vume\Controller {
+
+    public function getConfig()
+    {
+      $res = ["pusher_key" => pusher_key,
+              "pusher_id" => pusher_id,
+              "pusher_secret" => pusher_secret,
+              "username" => session("username")->get()];
+
+      echo json_encode($res);
     }
+  }
