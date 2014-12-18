25efbe33ecf19bd38d9971b779f1c3fd6930d8d3
devfake <devfakeplus@googlemail.com>
2014-12-18 15:51:42 +0100

    design login site

--- resources/views/login.php
+++ resources/views/login.php
@@ -1,7 +1,15 @@
-<form method="post" action="<?php echo URL; ?>login">
+<form method="post" action="<?php echo URL; ?>login" class="login">
 
-  <input type="text" name="name" placeholder="Benutzername" autofocus>
-  <input type="password" name="password">
+  <img src="<?php echo IMG_URL; ?>logo.png" width="91" height="57" alt="fume" class="logo">
+
+  <div>
+    <i class="sprite-user"></i>
+    <input type="text" name="name" placeholder="Username" autofocus>
+  </div>
+  <div>
+    <i class="sprite-password"></i>
+    <input type="password" name="password" placeholder="Password">
+  </div>
 
   <input type="submit" value="Anmelden">
 
