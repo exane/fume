00d3048ae3a7d20fcbf0a032315d13644976a9c4
devfake <devfakeplus@googlemail.com>
2014-12-21 12:37:26 +0100

    few improvements

--- resources/views/home.php
+++ resources/views/home.php
@@ -17,7 +17,7 @@
   <div class="chat-wrap">
     <div class="chats">
       <?php foreach($messages as $message): ?>
-        <div class="box <?php print $message->benutzer == $username ? 'box-me' : 'box-partner'; ?>">
+        <div class="box <?php echo $message->benutzer == $username ? 'box-me' : 'box-partner'; ?>">
           <p><?php echo $message->inhalt; ?></p>
           <span>
             <?php
@@ -29,6 +29,6 @@
       <?php endforeach; ?>
     </div>
     <textarea class="chatbox" placeholder="Schreib eine Nachricht..."
-              data-cookie="<?php echo session('username')->get(); ?>" autofocus></textarea>
+              data-cookie="<?php echo $username; ?>" autofocus></textarea>
   </div>
 </aside>
